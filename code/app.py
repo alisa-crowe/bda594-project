7faff7bc89b49b5fe671d029a842b1222c4805d2
alisa-crowe <acrowe3069@sdsu.edu>
2024-12-05 00:01:47 -0800

    setup for deploy of incident precition model

--- code/app.py
+++ code/app.py
@@ -1,15 +1,56 @@
 from flask import Flask, request, jsonify
+import joblib
 import pandas as pd
-import numpy as np
-from sklearn import tree
 
 app = Flask(__name__)
 
-# Load the data and train the model
-df = pd.read_csv("predictive-modeling/v11NumericIncidentPrediction.csv")
-feature_columns = ["Victim Age", "Overall Race", "Zip Code", "Domestic Violence Incident",
-                   "Hour", "Day of Week", "Day of Month", "Month"]
-target_column = "CIBRS Offense Description"
-X, y = df[feature_columns], df[target_column]
-clf = tree.DecisionTreeClassifier()
-clf = clf.fit(X, y)
+# Load the trained model
+model = joblib.load('decision_tree_model.pkl')
+
+# Define the mappings
+race_map = {
+    'HISPANIC': 0,
+    'WHITE': 1,
+    'OTHER': 2,
+    'BLACK': 3,
+    'ASIAN': 4,
+    'UNKNOWN': 5,
+    'PACIFIC ISLANDER': 6,
+    'AMERICAN INDIAN': 7
+}
+
+day_of_week_map = {
+    'MONDAY': 0,
+    'TUESDAY': 1,
+    'WEDNESDAY': 2,
+    'THURSDAY': 3,
+    'FRIDAY': 4,
+    'SATURDAY': 5,
+    'SUNDAY': 6
+}
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json(force=True)
+    # Map the input values to the corresponding numerical values
+    data['Overall Race'] = race_map[data['Overall Race']]
+    data['Day of Week'] = day_of_week_map[data['Day of Week']]
+
+    # Create a DataFrame for the input values
+    input_data = pd.DataFrame({
+        'Victim Age': [data['Victim Age']],
+        'Overall Race': [data['Overall Race']],
+        'Zip Code': [data['Zip Code']],
+        'Domestic Violence Incident': [data['Domestic Violence Incident']],
+        'Hour': [data['Hour']],
+        'Day of Week': [data['Day of Week']],
+        'Day of Month': [data['Day of Month']],
+        'Month': [data['Month']]
+    })
+
+    # Make a prediction
+    prediction = model.predict(input_data)
+    return jsonify({'prediction': prediction[0]})
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
