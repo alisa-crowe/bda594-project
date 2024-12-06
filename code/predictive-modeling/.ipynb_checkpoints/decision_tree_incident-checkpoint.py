46cb68341745609e9ccace4fbd3e4f94d6859b08
alisa-crowe <acrowe3069@sdsu.edu>
2024-12-05 17:44:53 -0800

    added test train split for incident precition

--- code/predictive-modeling/.ipynb_checkpoints/decision_tree_incident-checkpoint.py
+++ code/predictive-modeling/.ipynb_checkpoints/decision_tree_incident-checkpoint.py
@@ -1,6 +1,5 @@
 import pandas as pd
 from sklearn import tree
-from sklearn.model_selection import train_test_split
 
 # Read the data
 df = pd.read_csv("v11NumericIncidentPrediction.csv")
@@ -10,8 +9,9 @@ feature_columns = ["Victim Age", "Overall Race", "Zip Code", "Hour", "Day of Wee
 target_column = "CIBRS Offense Description"
 
 X, y = df[feature_columns], df[target_column]
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(X, y)
 
-# Map categorical columns
 race_map = {
     'HISPANIC': 0,
     'WHITE': 1,
@@ -22,6 +22,7 @@ race_map = {
     'PACIFIC ISLANDER': 6,
     'AMERICAN INDIAN': 7
 }
+
 day_of_week_map = {
     'MONDAY': 0,
     'TUESDAY': 1,
@@ -32,22 +33,6 @@ day_of_week_map = {
     'SUNDAY': 6
 }
 
-# Apply mappings to columns
-if 'Overall Race' in feature_columns:
-    X['Overall Race'] = X['Overall Race'].map(race_map)
-
-if 'Day of Week' in feature_columns:
-    X['Day of Week'] = X['Day of Week'].map(day_of_week_map)
-
-# Split into training and testing data
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-# Train the Decision Tree Classifier
-clf = tree.DecisionTreeClassifier()
-clf = clf.fit(X_train, y_train)
-
 # Save the updated model
 import joblib
 joblib.dump(clf, 'decision_tree_model.pkl')
-
-print("Model training complete. Saved as 'decision_tree_model.pkl'.")
